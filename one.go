d98e98c86486e2f7d7010fc92fa1c2e242888085
richard <ilixiang626@163.com>
2024-01-03 22:54:31 +0800

    feat: two sum;

---
+++ one.go
@@ -0,0 +1,14 @@
+package one
+
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+	for i, num := range nums {
+		diff := target - num
+		j, ok := m[diff]
+		if ok {
+			return []int{j, i}
+		}
+		m[num] = i
+	}
+	return []int{}
+}
