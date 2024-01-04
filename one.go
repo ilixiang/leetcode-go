29551f4a3f1ad873e71abb714c1ae00aca8f671c
richard <ilixiang626@163.com>
2024-01-04 08:09:28 +0800

    feat: addTwoNumbers;

--- one.go
+++ one.go
@@ -12,3 +12,43 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := new(ListNode)
+	n1, n2, tail := l1, l2, dummy
+	carry := 0
+	for nil != n1 && nil != n2 {
+		cur := new(ListNode)
+		cur.Val = (n1.Val + n2.Val + carry) % 10
+		carry = (n1.Val + n2.Val + carry) / 10
+		tail.Next = cur
+		n1, n2, tail = n1.Next, n2.Next, cur
+	}
+
+	var n *ListNode
+	if nil != n1 {
+		n = n1
+	} else {
+		n = n2
+	}
+
+	for nil != n {
+		cur := new(ListNode)
+		cur.Val = (n.Val + carry) % 10
+		carry = (n.Val + carry) / 10
+		tail.Next = cur
+		n, tail = n.Next, cur
+	}
+
+	if carry != 0 {
+		cur := new(ListNode)
+		cur.Val = carry
+		tail.Next = cur
+	}
+	return dummy.Next
+}
