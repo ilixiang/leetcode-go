946913006feabde716106e1ae1c6ebe97b3399e1
richard <ilixiang626@163.com>
2024-01-21 20:17:08 +0800

    feat: optimize 0009;

--- solutions/0009.palindrome-number/Solution.go
+++ solutions/0009.palindrome-number/Solution.go
@@ -5,16 +5,10 @@ func IsPalindrome(x int) bool {
 		return false
 	}
 
-	digits := make([]int, 0, 11)
-	for x != 0 {
-		digits = append(digits, x%10)
-		x /= 10
+	reversed := 0
+	original := x
+	for ; x != 0; x /= 10 {
+		reversed = reversed*10 + x%10
 	}
-
-	left, right := 0, len(digits)-1
-	for left < right && digits[left] == digits[right] {
-		left += 1
-		right -= 1
-	}
-	return left >= right
+	return original == reversed
 }
