e901663d7cf58a7abd64194e7a9f693f05c3c4ae
richard <ilixiang626@163.com>
2024-01-22 22:19:53 +0800

    feat: access slice seem faster;

--- solutions/0011.container-with-most-water/Solution.go
+++ solutions/0011.container-with-most-water/Solution.go
@@ -4,9 +4,8 @@ func MaxArea(height []int) int {
 	left, right := 0, len(height)-1
 	rev := 0
 	for left < right {
-		leftHeight, rightHeight := height[left], height[right]
-		rev = max(rev, min(leftHeight, rightHeight)*(right-left))
-		if leftHeight < rightHeight {
+		rev = max(rev, min(height[left], height[right])*(right-left))
+		if height[left] < height[right] {
 			left++
 		} else {
 			right--
