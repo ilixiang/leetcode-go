02bf97274c585dc52b2c4d1abf4411f91144c42d
richard <ilixiang626@163.com>
2024-01-30 08:36:38 +0800

    feat: 0029 42.92%;

---
+++ solutions/0029_DivideTwoIntegers.go
@@ -0,0 +1,44 @@
+package solutions
+
+func divide(dividend int, divisor int) int {
+	const MaxUint = ^uint32(0)
+	const MaxInt = int32(MaxUint >> 1)
+	const MinInt = -MaxInt - 1
+
+	if dividend == 0 {
+		return 0
+	}
+
+	positive := true
+	if dividend > 0 {
+		dividend = -dividend
+	} else {
+		positive = false
+	}
+	if divisor > 0 {
+		divisor = -divisor
+	} else {
+		if positive {
+			positive = false
+		} else {
+			positive = true
+		}
+	}
+
+	rev := 0
+	for dividend <= divisor {
+		leftShift := 0
+		for (divisor << leftShift) >= dividend {
+			leftShift++
+		}
+		rev -= 1 << (leftShift - 1)
+		dividend -= divisor << (leftShift - 1)
+	}
+	if positive && rev != int(MinInt) {
+		return 0 - rev
+	} else if positive {
+		return int(MaxInt)
+	} else {
+		return rev
+	}
+}
