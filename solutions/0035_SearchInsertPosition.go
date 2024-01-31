12a3f04a045fc7cb7dd23b85b1bc3f558df5f368
richard <ilixiang626@163.com>
2024-01-31 22:17:36 +0800

    feat: 0035 70.69%;

---
+++ solutions/0035_SearchInsertPosition.go
@@ -0,0 +1,16 @@
+package solutions
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	pos := len(nums)
+	for left <= right {
+		mid := (left + right) >> 1
+		if nums[mid] >= target {
+			right = mid - 1
+			pos = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return pos
+}
