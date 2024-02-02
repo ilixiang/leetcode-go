7b70e1faa88da26befb992fc1607e8dd1f598ac3
richard <ilixiang626@163.com>
2024-02-02 20:29:57 +0800

    feat: 0042 87.94%;

---
+++ solutions/0042_TrappingRainWater.go
@@ -0,0 +1,30 @@
+package solutions
+
+func trap(height []int) int {
+	if len(height) < 3 {
+		return 0
+	}
+
+	water := 0
+	left, right := 0, len(height)-1
+	maxLeftHeight, maxRightHeight := height[left], height[right]
+	for left < right {
+		if height[left] < maxLeftHeight {
+			water += maxLeftHeight - height[left]
+		} else {
+			maxLeftHeight = height[left]
+		}
+		if height[right] < maxRightHeight {
+			water += maxRightHeight - height[right]
+		} else {
+			maxRightHeight = height[right]
+		}
+
+		if maxLeftHeight < maxRightHeight {
+			left++
+		} else {
+			right--
+		}
+	}
+	return water
+}
