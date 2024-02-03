69006272f5b694435f6d72454a6dce0523361d64
richard <ilixiang626@163.com>
2024-02-03 21:28:22 +0800

    feat: 0048 40.76%;

---
+++ solutions/0048_RotateImage.go
@@ -0,0 +1,20 @@
+package solutions
+
+func rotate(matrix [][]int) {
+	size := len(matrix)
+
+	low, high := 0, size-1
+	for low < high {
+		for col := 0; col < size; col++ {
+			matrix[low][col], matrix[high][col] = matrix[high][col], matrix[low][col]
+		}
+		low++
+		high--
+	}
+
+	for row := 0; row < size; row++ {
+		for col := row + 1; col < size; col++ {
+			matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+		}
+	}
+}
