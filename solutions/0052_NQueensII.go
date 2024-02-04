c9c6245b36d9842448f12e06358a1ddf533401e8
richard <ilixiang626@163.com>
2024-02-04 09:35:00 +0800

    feat: 0052 81.99%;

---
+++ solutions/0052_NQueensII.go
@@ -0,0 +1,32 @@
+package solutions
+
+func totalNQueens(n int) int {
+	board := make([]int, n)
+
+	var recursive func(int) int
+	recursive = func(row int) int {
+		if row == n {
+			return 1
+		}
+
+		rev := 0
+		for col := 0; col < n; col++ {
+			mask := 1 << col
+			i := 0
+			for i < row &&
+				board[i]&mask == 0 &&
+				board[i]&(mask<<(row-i)) == 0 &&
+				board[i]&(mask>>(row-i)) == 0 {
+				i++
+			}
+			if i == row {
+				board[row] = mask
+				rev += recursive(row + 1)
+				board[row] = 0
+			}
+		}
+		return rev
+	}
+
+	return recursive(0)
+}
