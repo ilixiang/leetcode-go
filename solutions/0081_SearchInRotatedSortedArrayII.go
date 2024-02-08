a96f31f0e2c71deb22dfc882a4f4d68c491486b4
richard <ilixiang626@163.com>
2024-02-08 21:48:32 +0800

    feat: 0081 100%;

---
+++ solutions/0081_SearchInRotatedSortedArrayII.go
@@ -0,0 +1,39 @@
+package solutions
+
+func search(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) >> 1
+		if target == nums[mid] {
+			return true
+		} else if nums[left] == nums[right] {
+			if nums[left] == target {
+				return true
+			} else {
+				left++
+				right--
+			}
+		} else if target < nums[mid] {
+			if (nums[left] <= nums[mid] && nums[mid] <= nums[right]) || nums[left] > nums[mid] {
+				right = mid - 1
+			} else if nums[left] < target {
+				right = mid - 1
+			} else if nums[left] > target {
+				left = mid + 1
+			} else {
+				return true
+			}
+		} else {
+			if (nums[left] <= nums[mid] && nums[mid] <= nums[right]) || nums[mid] > nums[right] {
+				left = mid + 1
+			} else if nums[right] < target {
+				right = mid - 1
+			} else if nums[right] > target {
+				left = mid + 1
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
