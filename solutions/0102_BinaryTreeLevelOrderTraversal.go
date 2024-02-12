50295729a3b28516c66870aec948e2663cf6b728
richard <ilixiang626@163.com>
2024-02-12 20:27:32 +0800

    feat: 0102 100%;

---
+++ solutions/0102_BinaryTreeLevelOrderTraversal.go
@@ -0,0 +1,30 @@
+package solutions
+
+func LevelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 1)
+	queue[0] = root
+
+	for len(queue) != 0 {
+		buf := make([]int, 0)
+		count := len(queue)
+		for count != 0 {
+			node := queue[0]
+			buf = append(buf, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			queue = queue[1:]
+			count--
+		}
+		result = append(result, buf)
+	}
+	return result
+}
