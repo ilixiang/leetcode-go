ff4dc44be82e2696198d1f53841291a2c4f86512
richard <ilixiang626@163.com>
2024-02-14 13:01:46 +0800

    feat: 0110 52.52%;

---
+++ solutions/0110_BalancedBinaryTree.go
@@ -0,0 +1,21 @@
+package solutions
+
+func IsBalanced(root *TreeNode) bool {
+	var recursive func(*TreeNode) (bool, int)
+	recursive = func(root *TreeNode) (bool, int) {
+		if root == nil {
+			return true, 0
+		}
+
+		leftBalanced, leftHeight := recursive(root.Left)
+		if leftBalanced {
+			rightBalanced, rightHeight := recursive(root.Right)
+			heightDiff := leftHeight - rightHeight
+			balanced := leftBalanced && rightBalanced && -1 <= heightDiff && heightDiff <= 1
+			return balanced, max(leftHeight, rightHeight)
+		}
+		return false, 0
+	}
+	balanced, _ := recursive(root)
+	return balanced
+}
