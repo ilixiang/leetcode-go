1ea17a5f3212dc6684b47694f29b9951e3021edf
richard <ilixiang626@163.com>
2024-02-14 19:13:07 +0800

    feat: 0117 100%;

---
+++ solutions/0117_PopulatingNextRightPointersInEachNodeII.go
@@ -0,0 +1,28 @@
+package solutions
+
+func ConnectII(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	dummy := &Node{}
+	queue := make([]*Node, 1)
+	queue[0] = root
+	for len(queue) != 0 {
+		prev := dummy
+		for count := len(queue); count > 0; count-- {
+			node := queue[0]
+			queue = queue[1:]
+			prev.Next = node
+			prev = node
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		prev.Next = nil
+	}
+	return root
+}
