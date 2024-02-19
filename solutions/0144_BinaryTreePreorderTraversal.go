175a984f7778c03d31c7b80450667cb7dac78394
richard <ilixiang626@163.com>
2024-02-20 07:54:41 +0800

    feat 0144 74.21%;

---
+++ solutions/0144_BinaryTreePreorderTraversal.go
@@ -0,0 +1,21 @@
+package solutions
+
+func PreorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		res = append(res, node.Val)
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+	}
+	return res
+}
