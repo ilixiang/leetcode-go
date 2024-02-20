a39a0677412dddf60821fa0f1cf80f1a3dcd5011
richard <ilixiang626@163.com>
2024-02-20 08:01:16 +0800

    feat: 0145 100%;

---
+++ solutions/0145_BinaryTreePostorderTraversal.go
@@ -0,0 +1,30 @@
+package solutions
+
+func PostorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+	}
+
+	left, right := 0, len(res)-1
+	for left < right {
+		res[left], res[right] = res[right], res[left]
+		left++
+		right--
+	}
+	return res
+}
